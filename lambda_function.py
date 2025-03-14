d95c0b0865aa279e12d689f85cc0881f7f378bad
Nick <nickmihaly@gmail.com>
2025-03-13 18:31:40 -0700

    V2

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda V1!')
+        'body': json.dumps('Hello from Lambda V2!')
     }
 
